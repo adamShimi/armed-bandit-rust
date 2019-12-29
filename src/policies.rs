8d3f654ec60924f471bda6e174d1350a14c26c02
Adam Shimi <adam.shimi@toulouse-inp.fr>
2019-12-29 16:32:11 +0100

    Add Estimator trait and SampleAverage struct.

--- src/policies.rs
+++ src/policies.rs
@@ -8,11 +8,49 @@ mod policies {
   trait Policy {
     // Choose the action: either a lever for exploiting
     // or the exploring option.
-    fn decide() -> Action;
+    fn decide(&self) -> Action;
 
     // Update its values based on the result of the
     // step.
-    fn update(lever : usize, reward : f64);
+    fn update(&mut self, lever : usize, reward : f64);
   }
+
+  trait Estimator {
+    // Give the current estimate of the required lever.
+    fn estimate(&self, lever : usize) -> f64;
+
+    // Update the estimate of the lever according to
+    // the reward.
+    fn update(&mut self, lever : usize, reward : f64);
+  }
+
+  struct SampleAverage {
+    counter : f64,
+    estimates : Vec<f64>,
+  }
+
+  impl SampleAverage {
+
+    fn new(nb_levers : usize) -> Self {
+      SampleAverage {
+        counter : 1.0,
+        estimates : vec![0.0;nb_levers],
+      }
+    }
+  }
+
+  impl Estimator for SampleAverage {
+
+    fn estimate(&self, lever : usize) -> f64 {
+      self.estimates[lever]
+    }
+
+    fn update(&mut self, lever : usize, reward : f64) {
+      self.estimates[lever] =
+        self.estimates[lever] + (reward - self.estimates[lever])/self.counter;
+      self.counter += 1.0;
+    }
+  }
+
 
 }
