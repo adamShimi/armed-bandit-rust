2626d2011ddac0495a6f76ec7f540197e41147e1
Adam Shimi <adam.shimi@toulouse-inp.fr>
2019-12-29 16:14:51 +0100

    Add Policy trait and Action struct.

--- src/policies.rs
+++ src/policies.rs
@@ -1,2 +1,18 @@
 mod policies {
+
+  enum Action {
+    Explore,
+    Lever(usize),
+  }
+
+  trait Policy {
+    // Choose the action: either a lever for exploiting
+    // or the exploring option.
+    fn decide() -> Action;
+
+    // Update its values based on the result of the
+    // step.
+    fn update(lever : usize, reward : f64);
+  }
+
 }
