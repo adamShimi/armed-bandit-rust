4e3d1312b1f8bd9c85670669d07b1cd7298d8122
Adam Shimi <adam.shimi@toulouse-inp.fr>
2019-12-30 19:11:12 +0100

    Extract estimators into their own module.

--- src/policies.rs
+++ src/policies.rs
@@ -18,72 +18,75 @@ mod policies {
     fn update(&mut self, lever : usize, reward : f64);
   }
 
-  trait Estimator {
-    // Give the current estimate of the required lever.
-    fn estimate(&self, lever : usize) -> f64;
+  pub mod estimators {
 
-    // Find the list of levers with the best estimate.
-    fn optimal(&self) -> Vec<usize>;
+    pub trait Estimator {
+      // Give the current estimate of the required lever.
+      fn estimate(&self, lever : usize) -> f64;
 
-    // Update the estimate of the lever according to
-    // the reward.
-    fn update(&mut self, lever : usize, reward : f64);
-  }
+      // Find the list of levers with the best estimate.
+      fn optimal(&self) -> Vec<usize>;
 
-  struct SampleAverage {
-    counter : f64,
-    estimates : Vec<f64>,
-  }
+      // Update the estimate of the lever according to
+      // the reward.
+      fn update(&mut self, lever : usize, reward : f64);
+    }
 
-  impl SampleAverage {
+    pub struct SampleAverage {
+      pub counter : f64,
+      pub estimates : Vec<f64>,
+    }
 
-    fn new(nb_levers : usize) -> Self {
-      SampleAverage {
-        counter : 1.0,
-        estimates : vec![0.0;nb_levers],
+    impl SampleAverage {
+
+      pub fn new(nb_levers : usize) -> Self {
+        SampleAverage {
+          counter : 1.0,
+          estimates : vec![0.0;nb_levers],
+        }
       }
     }
-  }
 
-  impl Estimator for SampleAverage {
+    impl Estimator for SampleAverage {
 
-    fn estimate(&self, lever : usize) -> f64 {
-      self.estimates[lever]
-    }
+      fn estimate(&self, lever : usize) -> f64 {
+        self.estimates[lever]
+      }
 
-    fn optimal(&self) -> Vec<usize> {
-      self.estimates.iter()
-                    .enumerate()
-                    .fold((self.estimates[0],Vec::new()),
-                          |(mut max,mut occs), (nb,est)| {
-                            if *est > max {
-                              max = *est;
-                              occs.clear();
-                              occs.push(nb);
-                            } else if *est == max {
-                              occs.push(nb);
-                            }
-                            (max,occs)
-                    })
-                    .1
-    }
+      fn optimal(&self) -> Vec<usize> {
+        self.estimates.iter()
+                      .enumerate()
+                      .fold((self.estimates[0],Vec::new()),
+                            |(mut max,mut occs), (nb,est)| {
+                              if *est > max {
+                                max = *est;
+                                occs.clear();
+                                occs.push(nb);
+                              } else if *est == max {
+                                occs.push(nb);
+                              }
+                              (max,occs)
+                      })
+                      .1
+      }
 
-    fn update(&mut self, lever : usize, reward : f64) {
-      self.estimates[lever] =
-        self.estimates[lever] + (reward - self.estimates[lever])/self.counter;
-      self.counter += 1.0;
+      fn update(&mut self, lever : usize, reward : f64) {
+        self.estimates[lever] =
+          self.estimates[lever] + (reward - self.estimates[lever])/self.counter;
+        self.counter += 1.0;
+      }
     }
   }
 
   struct EGreedy {
     nb_levers : usize,
     expl_proba : f64,
-    estimator : Box<dyn Estimator>,
+    estimator : Box<dyn estimators::Estimator>,
   }
 
   impl EGreedy {
 
-    fn new(nb_levers : usize, expl_proba: f64, estimator : Box<dyn Estimator>) -> Self {
+    fn new(nb_levers : usize, expl_proba: f64, estimator : Box<dyn estimators::Estimator>) -> Self {
       EGreedy {
         nb_levers,
         expl_proba,
