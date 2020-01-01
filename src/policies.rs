278481cf25d4f29fb9588d12f8be912207c4bbd3
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-01 19:14:32 +0100

    Remove outer module (redundant).

--- src/policies.rs
+++ src/policies.rs
@@ -1,205 +1,201 @@
-pub mod policies {
+use rand::Rng;
+use rand::prelude::IteratorRandom;
 
-  use rand::Rng;
-  use rand::prelude::IteratorRandom;
+pub trait Policy {
+  // Choose the action: either a lever for exploiting
+  // or the exploring option.
+  fn decide(&self) -> usize;
 
-  trait Policy {
-    // Choose the action: either a lever for exploiting
-    // or the exploring option.
-    fn decide(&self) -> usize;
-
-    // Update its values based on the result of the
-    // step.
-    fn update(&mut self, lever : usize, reward : f64);
-  }
+  // Update its values based on the result of the
+  // step.
+  fn update(&mut self, lever : usize, reward : f64);
+}
 
 
-  pub struct EGreedy {
-    nb_levers : usize,
-    expl_proba : f64,
-    estimator : Box<dyn estimators::Estimator>,
-  }
+pub struct EGreedy {
+  nb_levers : usize,
+  expl_proba : f64,
+  estimator : Box<dyn estimators::Estimator>,
+}
 
-  impl EGreedy {
+impl EGreedy {
 
-    pub fn new(nb_levers : usize,
-               expl_proba: f64,
-               estimator : Box<dyn estimators::Estimator>) -> Self {
-      EGreedy {
-        nb_levers,
-        expl_proba,
-        estimator,
-      }
-    }
-
-
-    fn explore(&self) -> usize {
-      rand::thread_rng().gen_range(0,self.nb_levers)
+  pub fn new(nb_levers : usize,
+             expl_proba: f64,
+             estimator : Box<dyn estimators::Estimator>) -> Self {
+    EGreedy {
+      nb_levers,
+      expl_proba,
+      estimator,
     }
   }
 
-  impl Policy for EGreedy {
 
-    fn decide(&self) -> usize {
-      if rand::thread_rng().gen_bool(self.expl_proba) {
-        self.explore()
-      } else {
-        *self.estimator.optimal(self.nb_levers)
-                       .iter()
-                       .choose(&mut rand::thread_rng())
-                       .unwrap()
-      }
-    }
-
-    // Update its values based on the result of the
-    // step.
-    fn update(&mut self, lever : usize, reward : f64) {
-      self.estimator.update(lever,reward);
-    }
+  fn explore(&self) -> usize {
+    rand::thread_rng().gen_range(0,self.nb_levers)
   }
+}
 
-  pub struct UCB {
-    nb_levers : usize,
-    time : f64,
-    counts : Vec<f64>,
-    estimator : Box<dyn estimators::Estimator>,
-  }
+impl Policy for EGreedy {
 
-  impl UCB {
-
-    pub fn new(nb_levers : usize, estimator : Box<dyn estimators::Estimator>) -> Self {
-      UCB {
-        nb_levers,
-        time : 0.0,
-        counts : vec![0.0;nb_levers],
-        estimator,
-      }
-    }
-  }
-
-  impl Policy for UCB {
-
-    fn decide(&self) -> usize {
-      *self.estimator.all(self.nb_levers)
-                     .iter()
-                     .zip(self.counts.iter())
-                     .enumerate()
-                     .fold((self.estimator.estimate(0),Vec::new()),
-                           |(mut max,mut occs), (nb,(est,count))| {
-                             let val = *est + (2.0*self.time.log(2.0) / *count).sqrt();
-                             if val > max {
-                               max = val;
-                               occs.clear();
-                               occs.push(nb);
-                             } else if val == max {
-                               occs.push(nb);
-                             }
-                             (max,occs)
-                     })
-                     .1
+  fn decide(&self) -> usize {
+    if rand::thread_rng().gen_bool(self.expl_proba) {
+      self.explore()
+    } else {
+      *self.estimator.optimal(self.nb_levers)
                      .iter()
                      .choose(&mut rand::thread_rng())
                      .unwrap()
     }
+  }
 
-    // Update its values based on the result of the
-    // step.
+  // Update its values based on the result of the
+  // step.
+  fn update(&mut self, lever : usize, reward : f64) {
+    self.estimator.update(lever,reward);
+  }
+}
+
+pub struct UCB {
+  nb_levers : usize,
+  time : f64,
+  counts : Vec<f64>,
+  estimator : Box<dyn estimators::Estimator>,
+}
+
+impl UCB {
+
+  pub fn new(nb_levers : usize, estimator : Box<dyn estimators::Estimator>) -> Self {
+    UCB {
+      nb_levers,
+      time : 0.0,
+      counts : vec![0.0;nb_levers],
+      estimator,
+    }
+  }
+}
+
+impl Policy for UCB {
+
+  fn decide(&self) -> usize {
+    *self.estimator.all(self.nb_levers)
+                   .iter()
+                   .zip(self.counts.iter())
+                   .enumerate()
+                   .fold((self.estimator.estimate(0),Vec::new()),
+                         |(mut max,mut occs), (nb,(est,count))| {
+                           let val = *est + (2.0*self.time.log(2.0) / *count).sqrt();
+                           if val > max {
+                             max = val;
+                             occs.clear();
+                             occs.push(nb);
+                           } else if val == max {
+                             occs.push(nb);
+                           }
+                           (max,occs)
+                   })
+                   .1
+                   .iter()
+                   .choose(&mut rand::thread_rng())
+                   .unwrap()
+  }
+
+  // Update its values based on the result of the
+  // step.
+  fn update(&mut self, lever : usize, reward : f64) {
+    self.estimator.update(lever,reward);
+  }
+}
+
+pub mod estimators {
+
+  pub trait Estimator {
+    // Give the current estimate of the required lever.
+    fn estimate(&self, lever : usize) -> f64;
+
+    // Update the estimate of the lever according to
+    // the reward.
+    fn update(&mut self, lever : usize, reward : f64);
+
+    // Give all estimates.
+    fn all(&self, nb_levers : usize) -> Vec<f64> {
+      (0..nb_levers).map(|x| self.estimate(x))
+                    .collect()
+    }
+
+    // Find the list of levers with the best estimate.
+    fn optimal(&self, nb_levers : usize) -> Vec<usize> {
+      self.all(nb_levers)
+          .iter()
+          .enumerate()
+          .fold((self.estimate(0),Vec::new()),
+                |(mut max,mut occs), (nb,est)| {
+                  if *est > max {
+                    max = *est;
+                    occs.clear();
+                    occs.push(nb);
+                  } else if *est == max {
+                    occs.push(nb);
+                  }
+                  (max,occs)
+          })
+          .1
+    }
+  }
+
+  pub struct SampleAverage {
+    pub counter : f64,
+    pub estimates : Vec<f64>,
+  }
+
+  impl SampleAverage {
+
+    pub fn new(nb_levers : usize) -> Self {
+      SampleAverage {
+        counter : 1.0,
+        estimates : vec![0.0;nb_levers],
+      }
+    }
+  }
+
+  impl Estimator for SampleAverage {
+
+    fn estimate(&self, lever : usize) -> f64 {
+      self.estimates[lever]
+    }
+
     fn update(&mut self, lever : usize, reward : f64) {
-      self.estimator.update(lever,reward);
+      self.estimates[lever] =
+        self.estimates[lever] + (reward - self.estimates[lever])/self.counter;
+      self.counter += 1.0;
     }
   }
 
-  pub mod estimators {
 
-    pub trait Estimator {
-      // Give the current estimate of the required lever.
-      fn estimate(&self, lever : usize) -> f64;
+  pub struct ConstantStep {
+    pub step : f64,
+    pub estimates : Vec<f64>,
+  }
 
-      // Update the estimate of the lever according to
-      // the reward.
-      fn update(&mut self, lever : usize, reward : f64);
+  impl ConstantStep {
 
-      // Give all estimates.
-      fn all(&self, nb_levers : usize) -> Vec<f64> {
-        (0..nb_levers).map(|x| self.estimate(x))
-                      .collect()
-      }
-
-      // Find the list of levers with the best estimate.
-      fn optimal(&self, nb_levers : usize) -> Vec<usize> {
-        self.all(nb_levers)
-            .iter()
-            .enumerate()
-            .fold((self.estimate(0),Vec::new()),
-                  |(mut max,mut occs), (nb,est)| {
-                    if *est > max {
-                      max = *est;
-                      occs.clear();
-                      occs.push(nb);
-                    } else if *est == max {
-                      occs.push(nb);
-                    }
-                    (max,occs)
-            })
-            .1
-      }
-    }
-
-    pub struct SampleAverage {
-      pub counter : f64,
-      pub estimates : Vec<f64>,
-    }
-
-    impl SampleAverage {
-
-      pub fn new(nb_levers : usize) -> Self {
-        SampleAverage {
-          counter : 1.0,
-          estimates : vec![0.0;nb_levers],
-        }
-      }
-    }
-
-    impl Estimator for SampleAverage {
-
-      fn estimate(&self, lever : usize) -> f64 {
-        self.estimates[lever]
-      }
-
-      fn update(&mut self, lever : usize, reward : f64) {
-        self.estimates[lever] =
-          self.estimates[lever] + (reward - self.estimates[lever])/self.counter;
-        self.counter += 1.0;
-      }
-    }
-
-
-    pub struct ConstantStep {
-      pub step : f64,
-      pub estimates : Vec<f64>,
-    }
-
-    impl ConstantStep {
-
-      pub fn new(nb_levers : usize, step : f64) -> Self {
-        ConstantStep {
-          step,
-          estimates : vec![0.0;nb_levers],
-        }
-      }
-    }
-
-    impl Estimator for ConstantStep {
-
-      fn estimate(&self, lever : usize) -> f64 {
-        self.estimates[lever]
-      }
-
-      fn update(&mut self, lever : usize, reward : f64) {
-        self.estimates[lever] =
-          self.estimates[lever] + self.step*(reward - self.estimates[lever]);
+    pub fn new(nb_levers : usize, step : f64) -> Self {
+      ConstantStep {
+        step,
+        estimates : vec![0.0;nb_levers],
       }
     }
   }
 
+  impl Estimator for ConstantStep {
+
+    fn estimate(&self, lever : usize) -> f64 {
+      self.estimates[lever]
+    }
+
+    fn update(&mut self, lever : usize, reward : f64) {
+      self.estimates[lever] =
+        self.estimates[lever] + self.step*(reward - self.estimates[lever]);
+    }
+  }
 }
--- src/problems.rs
+++ src/problems.rs
@@ -1,86 +1,83 @@
-pub mod problems {
+use rand_distr::{Normal, Distribution};
 
-  use rand_distr::{Normal, Distribution};
+pub trait Bandit {
+  // Get reward from a lever.
+  fn use_lever(&mut self, lever : usize) -> f64;
+}
 
-  pub trait Bandit {
-    // Get reward from a lever.
-    fn use_lever(&mut self, lever : usize) -> f64;
+
+// Implementatio of a stationary bandit instance, where
+// nb_levers levers are initialized at according to a normal distribution.
+pub struct BanditStationary {
+  levers : Vec<Normal<f64>>,
+}
+
+impl BanditStationary {
+
+  pub fn new( nb_levers : usize, init : (f64,f64)) -> Self {
+    let init_distrib = Normal::new(init.0,init.1).unwrap();
+    BanditStationary {
+      levers : (0..nb_levers).map(|_|
+                   Normal::new(init_distrib.sample(&mut rand::thread_rng()),
+                         init.1)
+                       .unwrap()
+                 )
+                 .collect()
+    }
+  }
+}
+
+impl Bandit for BanditStationary {
+
+  fn use_lever(&mut self, lever: usize) -> f64 {
+    self.levers[lever].sample(&mut rand::thread_rng())
   }
 
+}
 
-  // Implementatio of a stationary bandit instance, where
-  // nb_levers levers are initialized at according to a normal distribution.
-  pub struct BanditStationary {
-    levers : Vec<Normal<f64>>,
-  }
 
-  impl BanditStationary {
 
-    pub fn new( nb_levers : usize, init : (f64,f64)) -> Self {
-      let init_distrib = Normal::new(init.0,init.1).unwrap();
-      BanditStationary {
-        levers : (0..nb_levers).map(|_|
-                     Normal::new(init_distrib.sample(&mut rand::thread_rng()),
-                           init.1)
-                         .unwrap()
-                   )
-                   .collect()
-      }
+
+// Implementation of a nonstationary bandit problems, where
+// levers start with the same mean and move according to a random
+// walk at each step.
+pub struct BanditNonStationary {
+  levers : Vec<f64>,
+  // Only one std, because only the means move according to
+  // the random walk; the standard deviation stays the same.
+  std : f64,
+  walks : Vec<Normal<f64>>,
+}
+
+impl BanditNonStationary {
+
+  pub fn new(nb_levers : usize, init : (f64,f64), walk : (f64,f64)) -> Self {
+    BanditNonStationary {
+      levers : vec![init.0; nb_levers],
+      std : init.1,
+      walks : (1..nb_levers).map(|_| Normal::new(walk.0,walk.1).unwrap())
+                 .collect(),
     }
   }
 
-  impl Bandit for BanditStationary {
+  fn update(&mut self) {
+    self.levers =
+      self.levers.iter()
+           .zip(self.walks.iter())
+           .map(|(lever,walk)| lever + walk.sample(&mut rand::thread_rng()))
+           .collect();
+  }
+}
 
-    fn use_lever(&mut self, lever: usize) -> f64 {
-      self.levers[lever].sample(&mut rand::thread_rng())
-    }
+impl Bandit for BanditNonStationary {
+
+  fn use_lever(&mut self, lever: usize) -> f64 {
+    let result = Normal::new(self.levers[lever],self.std)
+             .unwrap()
+             .sample(&mut rand::thread_rng());
+    self.update();
+    result
 
   }
 
-
-
-
-  // Implementation of a nonstationary bandit problems, where
-  // levers start with the same mean and move according to a random
-  // walk at each step.
-  pub struct BanditNonStationary {
-    levers : Vec<f64>,
-    // Only one std, because only the means move according to
-    // the random walk; the standard deviation stays the same.
-    std : f64,
-    walks : Vec<Normal<f64>>,
-  }
-
-  impl BanditNonStationary {
-
-    pub fn new(nb_levers : usize, init : (f64,f64), walk : (f64,f64)) -> Self {
-      BanditNonStationary {
-        levers : vec![init.0; nb_levers],
-        std : init.1,
-        walks : (1..nb_levers).map(|_| Normal::new(walk.0,walk.1).unwrap())
-                   .collect(),
-      }
-    }
-
-    fn update(&mut self) {
-      self.levers =
-        self.levers.iter()
-             .zip(self.walks.iter())
-             .map(|(lever,walk)| lever + walk.sample(&mut rand::thread_rng()))
-             .collect();
-    }
-  }
-
-  impl Bandit for BanditNonStationary {
-
-    fn use_lever(&mut self, lever: usize) -> f64 {
-      let result = Normal::new(self.levers[lever],self.std)
-               .unwrap()
-               .sample(&mut rand::thread_rng());
-      self.update();
-      result
-
-    }
-
-  }
 }
