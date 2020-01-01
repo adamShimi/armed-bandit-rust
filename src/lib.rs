2df536f9e9b254901f3c73225d8f15d2944805fb
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-01 19:19:33 +0100

    Add Experiment and implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,38 @@ extern crate rand_distr;
 pub mod problems;
 pub mod policies;
 
+pub struct Experiment {
+  problem : Box<dyn problems::Bandit>,
+  policy : Box<dyn policies::Policy>,
+}
+
+impl Experiment {
+
+  pub fn new(problem : Box<dyn problems::Bandit>,
+         policy : Box<dyn policies::Policy>) -> Self {
+    Experiment {
+      problem,
+      policy,
+    }
+  }
+
+  pub fn step(&mut self) -> Step {
+    let lever = self.policy.decide();
+    let reward = self.problem.use_lever(lever);
+    self.policy.update(lever,reward);
+    Step {
+      lever,
+      reward,
+    }
+  }
+
+}
+
+pub struct Step {
+  pub lever : usize,
+  pub reward : f64,
+}
+
 #[cfg(test)]
 mod tests {
   #[test]
