c4917d9e873e22f6a19de3f2b1b1c1ccedf5d33e
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-08 18:15:48 +0100

    Remove boxes from the integration test.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,8 +6,8 @@ use policies::estimators;
 fn experiment() {
   let nb_levers = 10;
   let est = estimators::SampleAverage::new(nb_levers);
-  let policy = policies::EGreedy::new(nb_levers,0.1,Box::new(est));
+  let policy = policies::EGreedy::new(nb_levers,0.1,est);
   let problem = problems::BanditStationary::new(nb_levers,(0.0,1.0));
-  let experiment = bandit_rs::Experiment::new(Box::new(problem),Box::new(policy));
+  let experiment = bandit_rs::Experiment::new(problem,policy);
   bandit_rs::run_experiments(experiment,2000,1000);
 }
