a1e8225edf44a3d56c8e5437b34534a351e64c80
Adam Shimi <adam.shimi@toulouse-inp.fr>
2020-01-09 18:54:18 +0100

    Adapt integration test.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,17 +3,39 @@ use bandit_rs::{policies,problems};
 use policies::estimators;
 
 const NB_LEVERS:usize = 10;
-const EPS:f64 = 0.1;
 const NB_TRIES:usize = 2000;
-const LEN_EXP:usize = 2000;
+const LEN_EXP:usize = 1000;
 const GAUSS:(f64,f64) = (0.0,1.0);
+const EPS:f64 = 0.1;
+const NAME: &str = "e = 0.1, sample average";
+const EPS2:f64 = 0.01;
+const NAME2: &str = "e = 0.01, sample average";
+const EPS3:f64 = 0.0;
+const NAME3: &str = "e = 0, sample average";
 
 #[test]
 fn experiment() {
+
+  let problems : Vec<problems::BanditStationary> =
+    (0..NB_TRIES).map( |_| problems::BanditStationary::new(NB_LEVERS,GAUSS))
+                 .collect();
   let est = estimators::SampleAverage::new(NB_LEVERS);
-  let policy = policies::EGreedy::new(NB_LEVERS,EPS,est);
-  let problem = problems::BanditStationary::new(NB_LEVERS,GAUSS);
-  let experiment = bandit_rs::Experiment::new(problem,policy);
-  let results = bandit_rs::run_experiment(experiment,NB_TRIES,LEN_EXP);
-  bandit_rs::plot_optimal_percentage(results,NB_TRIES,LEN_EXP);
+
+  let mut policies = Vec::new();
+  policies.push(policies::EGreedy::new(NB_LEVERS,EPS,est.clone()));
+  policies.push(policies::EGreedy::new(NB_LEVERS,EPS2,est.clone()));
+  policies.push(policies::EGreedy::new(NB_LEVERS,EPS3,est.clone()));
+
+  let results : Vec<Vec<f64>> =
+    bandit_rs::run_experiment(policies,problems,NB_TRIES,LEN_EXP)
+              .into_iter()
+              .map(|result| bandit_rs::optimal_percentage(result,NB_TRIES,LEN_EXP))
+              .collect();
+
+  let mut names = Vec::new();
+  names.push(NAME);
+  names.push(NAME2);
+  names.push(NAME3);
+
+  bandit_rs::plot_results(names.into_iter().zip(results.into_iter()).collect(), LEN_EXP);
 }
